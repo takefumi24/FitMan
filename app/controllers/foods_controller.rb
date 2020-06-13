3fd6aafbeb97df714299065ecf880a08432e73ca
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-13 12:05:19 +0900

    before_action :create_hashを追加
    indexビューで使用するため

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,16 @@
 class FoodsController < ApplicationController
+  before_action :create_hash
+
+
   def index
   end
+
+  private
+  def create_hash
+    @meals = ["朝食","昼食","夕食","間食"]
+    @foods = ["チキンラーメン","はなまるうどん","骨付きステーキ"]
+    @nutrition = [{ja:"カロリー", eng:"kcal"},{ja:"炭水化物", eng:"g"},{ja:"脂肪", eng:"g"},{ja:"タンパク質", eng:"g"}]
+    @data = [100,80,60,40]
+  end
+
 end
