e647b01b000e2edb931abf816c79a1943e623b01
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-19 19:12:38 +0900

    【コントローラー】@mealsにidを付与

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -19,7 +19,8 @@ class FoodsController < ApplicationController
 
 
   def create_hash
-    @meals = ["朝食","昼食","夕食","間食"]
+    @meals = [{name:"朝食", id:"1"}, {name:"昼食", id:"2"}, {name:"夕食", id:"3"}, {name:"間食", id:"4"}]
+
     @nutrition = [{ja:"カロリー", eng:"kcal", column:"calorie"},{ja:"炭水化物", eng:"g", column:"carbo"},{ja:"脂肪", eng:"g", column:"fat"},{ja:"タンパク質", eng:"g", column:"protein"}]
   end
 
