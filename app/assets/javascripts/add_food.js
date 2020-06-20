5ecb004039f3d0ba8d1a70b6d954e2003426d741
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-20 21:31:56 +0900

    追加ボタンクリックでデータを取得

--- app/assets/javascripts/add_food.js
+++ app/assets/javascripts/add_food.js
@@ -1,4 +1,10 @@
 $(function () {
+
+  // function buildHTML(message){
+  //   let html = `
+  //   `
+  // }
+
   // フード追加モーダル
   $(".add").click(function (e) {
     var addId = this.id;
@@ -6,12 +12,25 @@ $(function () {
     $(".modal-overlay").fadeIn("fast");
     $(".modal_foods").fadeIn("fast");
 
-    $(".fa-plus-circle").click(function () {
-      console.log("ok");
-      $(".modal-overlay").fadeOut("fast");
-      $(".modal_foods").fadeOut("fast");
-    });
   });
+
+  // 追加ボタンクリック
+  $('.fa-plus-circle').click(function(e){
+    e.preventDefault();
+    let food_id = this.id
+    let nutrition = document.getElementById('nutrition' + food_id).children
+    let data = {
+      name   :document.getElementById('name' + food_id).innerHTML,
+      calorie:nutrition[0].textContent,
+      carbo  :nutrition[1].textContent,
+      fat    :nutrition[2].textContent,
+      protein:nutrition[3].textContent,
+    }
+    let food_list = buildHTML(data);
+    $('.modal-overlay').fadeOut("fast");
+    $('.modal_foods').fadeOut("fast");
+  });
+
 
   // モーダルを閉じる
   $(".modal-overlay").click(function () {
