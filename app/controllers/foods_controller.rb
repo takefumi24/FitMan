f7c3b2434a42ff8abb9b575c66f4744b0b957723
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-21 14:34:17 +0900

    【foodコントローラー】ajaxのため変更

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,31 +1,26 @@
 class FoodsController < ApplicationController
   before_action :create_hash
-
-
   def index
     @foods = Food.all
   end
 
-  def crate
+  def create
     @food = Food.new(food_params)
-    @food.save
-    respond_to do |format|
-      format.html
-      format.json
+    if @food.save
+      respond_to do |format|
+        format.json
+      end
+    else
+      redirect_to foods_path
     end
-    redirect_to foods_path
   end
+
   private
   def food_params
     params.permit(:name, :quantity, :calorie, :carbo, :fat, :protein)
   end
-
-
-
   def create_hash
     @meals = [{name:"朝食", id:"1"}, {name:"昼食", id:"2"}, {name:"夕食", id:"3"}, {name:"間食", id:"4"}]
-
     @nutrition = [{ja:"カロリー", eng:"kcal", column:"calorie"},{ja:"炭水化物", eng:"g", column:"carbo"},{ja:"脂肪", eng:"g", column:"fat"},{ja:"タンパク質", eng:"g", column:"protein"}]
   end
-
 end
