0abc0a569d3d7b0801e89c6480761cf52d1012f4
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-21 13:45:59 +0900

    フード追加ごとにそれぞれの食事でカロリーを合計

--- app/assets/javascripts/add_food.js
+++ app/assets/javascripts/add_food.js
@@ -1,4 +1,10 @@
 $(function () {
+
+  let meal1 = [] //朝食
+  let meal2 = [] //昼食
+  let meal3 = [] //夕食
+  let meal4 = [] //間食
+
   // フードリストの追加
   function buildHTML(data){
     let food_list = `
@@ -6,17 +12,17 @@ $(function () {
                       <div class="foods_main__name">
                         ${data.name}
                       </div>
-                      <ul class="foods_main__index" id="nutrition6">
-                        <li class="foods_main__index">
+                      <ul class="foods_main__index">
+                        <li class="foods_main__index--num ${data.class}calorie">
                           ${data.calorie}
                         </li>
-                        <li class="foods_main__index--num">
+                        <li class="foods_main__index--num ${data.class}calorie">
                           ${data.carbo}
                         </li>
-                        <li class="foods_main__index--num">
+                        <li class="foods_main__index--num ${data.class}calorie">
                           ${data.fat}
                         </li>
-                        <li class="foods_main__index--num">
+                        <li class="foods_main__index--num ${data.class}calorie">
                           ${data.protein}
                         </li>
                         <div class="foods_main__remove">
@@ -32,6 +38,7 @@ $(function () {
   $(".add").click(function (e) {
     e.preventDefault();
     let addId = this.id;
+    let mealClass = "meal" + addId;
     $(".modal-overlay").fadeIn("fast");
     $(".modal_foods").fadeIn("fast");
     $(".fa-plus-circle").off();
@@ -41,15 +48,54 @@ $(function () {
       let food_id = this.id;
       let nutrition = document.getElementById("nutrition" + food_id).children;
       let data = {
+        class: mealClass,
         name: document.getElementById("name" + food_id).innerHTML,
-        calorie: nutrition[0].textContent,
-        carbo: nutrition[1].textContent,
-        fat: nutrition[2].textContent,
-        protein: nutrition[3].textContent,
+        calorie: Number(nutrition[0].textContent),
+        carbo: Number(nutrition[1].textContent),
+        fat: Number(nutrition[2].textContent),
+        protein: Number(nutrition[3].textContent),
       };
-      let food_list = buildHTML(data);
+      // 追加先の食事のhtml
       let mealId = "#foods" + addId;
+      // htmlを作成
+      let food_list = buildHTML(data);
+
       $(mealId).append(food_list);
+
+      // 追加する配列を指定
+      if (addId == 1) {
+        array = meal1
+      } else if (addId == 2) {
+        array = meal2
+      } else if (addId == 3) {
+        array = meal3
+      } else if (addId == 4) {
+        array = meal4
+      }
+
+      // 配列に追加されたフードのデータを追加
+      array.push(data);
+
+      // それぞれのデータを合計
+      let sumCalorie = array.reduce((p, x) => p + x.calorie, 0)
+      let sumCarbo   = array.reduce((p, x) => p + x.carbo, 0)
+      let sumFat     = array.reduce((p, x) => p + x.fat, 0)
+      let sumProtein = array.reduce((p, x) => p + x.protein, 0)
+
+      // 追加先の食事
+      let mealTotal = '#meal' + addId
+      let calorieTotal = $(mealTotal).children('.data_calorie')
+      let carboTotal   = $(mealTotal).children('.data_carbo')
+      let fatTotal     = $(mealTotal).children('.data_fat')
+      let proteinTotal = $(mealTotal).children('.data_protein')
+
+      // 数値の合計部分
+      $(calorieTotal).text(sumCalorie);
+      $(carboTotal).text(sumCarbo);
+      $(fatTotal).text(sumFat);
+      $(proteinTotal).text(sumProtein);
+
+      // // フード追加ごとにモーダルを削除するか検討中
       // $(".modal-overlay").fadeOut("fast");
       // $(".modal_foods").fadeOut("fast");
     });
@@ -70,9 +116,4 @@ $(function () {
     $(removeList).remove();
     $('.fa-minus-circle').off();
   })
-  // 追加されているフードの数値計算
-  let addFoodCalculate = document.getElementsByClassName('modal_foods__index__list__data--num');
-  console.log(addFoodCalculate);
-  let text = addFoodCalculate[0].innerHTML;
-  console.log(text);
 });
