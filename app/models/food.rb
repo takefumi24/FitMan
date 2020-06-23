064229a06ebb169e0474f454c9f00622ff55fc00
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-23 14:27:52 +0900

    searchメソッドを追加

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,6 +3,11 @@ class Food < ApplicationRecord
   validates :quantity,         presence: true
   validates :one_of_nutrition, presence: true, format: {with: /\A[0-9]+\z/ }, inclusion: 0..999
 
+  def self.search(input)
+    return nil if input == ""
+    Food.where(['name LIKE ?', "%#{input}%"] )
+  end
+
   private
     def one_of_nutrition
       calorie.presence or carbo.presence or fat.presence or protein.presence
