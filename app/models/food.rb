3cd46527dd32fbf5e120e348203c13c53ef721e0
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-24 21:16:41 +0900

    栄養のフォームが空の場合には０とする

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,12 +3,20 @@ class Food < ApplicationRecord
   validates :quantity,         presence: true
   validates :one_of_nutrition, presence: true, format: {with: /\A[0-9]+\z/ }, inclusion: 0..999
 
+# インクリメンタルサーチ
   def self.search(input)
     if input == ""
       Food.all
     else
       Food.where(['name LIKE ?', "%#{input}%"] )
     end
+  end
+
+  def column_check
+    self.calorie = 0 if self.calorie.blank?
+    self.carbo   = 0 if self.carbo.blank?
+    self.fat     = 0 if self.fat.blank?
+    self.protein = 0 if self.protein.blank?
   end
 
   private
