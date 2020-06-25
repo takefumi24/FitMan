a0df0f0f995e9379b5edbb95f30ad1247e9f7d72
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-25 15:04:24 +0900

    カラムにnicknameを追加

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_06_24_140627) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
