1f0655a217f73ecbb68713a0817abf23317b1c20
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-25 17:41:09 +0900

    バリデーションの設定

--- app/models/user_datum.rb
+++ app/models/user_datum.rb
@@ -1,4 +1,16 @@
 class UserDatum < ApplicationRecord
+  belongs_to :user
+
+  validates :gender,         presence: true
+  validates :birthday,       presence: true
+  validates :weight,         presence: true
+  validates :life_index_id,  presence: true
+  validates :burn_calorie,   presence: true
+  validates :goal_weight,    presence: true
+  validates :goal_period,    presence: true
+  validates :intake_calorie, presence: true
+  validates :user_id,        presence: true
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :life_index
 end
