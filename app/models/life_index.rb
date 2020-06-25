ceaced145f09df00d90fdd44f97f9324af09fa2b
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-25 17:36:01 +0900

    life_indexモデルの記述修正

--- app/models/life_index.rb
+++ app/models/life_index.rb
@@ -1,5 +1,5 @@
-class Life_index < ActiveHash::Base
+class LifeIndex < ActiveHash::Base
   self.data = [
-    {id:1, index:1.3}, {id:2 index:1.5}, {id:3, index:1.7}, {id:4, index:1.9},
+    {id:1, index:1.3}, {id:2,index:1.5}, {id:3, index:1.7}, {id:4, index:1.9}
   ]
 end
