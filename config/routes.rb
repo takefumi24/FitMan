79508f2c77c9510b488550b5ca6698ef393d8931
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-25 16:15:59 +0900

    ユーザーマイページ完成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "users#index"
-  resources :users, only: :index
+  resources :users, only: [:index, :show]
 
   resources :foods, only: [:index,:create]
 
