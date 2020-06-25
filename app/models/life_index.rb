c6034485b59a11975773b0606b62030e4f8ee0a2
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-25 17:04:35 +0900

    active_hash作成と適用

---
+++ app/models/life_index.rb
@@ -0,0 +1,5 @@
+class Life_index < ActiveHash::Base
+  self.data = [
+    {id:1, index:1.3}, {id:2 index:1.5}, {id:3, index:1.7}, {id:4, index:1.9},
+  ]
+end
--- app/models/user_datum.rb
+++ app/models/user_datum.rb
@@ -1,2 +1,4 @@
 class UserDatum < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :life_index
 end
